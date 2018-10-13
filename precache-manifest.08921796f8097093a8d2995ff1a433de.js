137d0f59ffd1b22281e553e532ac06e681b509ea
AmirSalar <amirsalar.p.d74@gmail.com>
2018-10-13 22:41:02 +0330

    Updates

---
+++ precache-manifest.08921796f8097093a8d2995ff1a433de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce0302b16508312457e7",
+    "url": "/Todo-app/static/js/runtime~main.ce0302b1.js"
+  },
+  {
+    "revision": "59c891266339c37df121",
+    "url": "/Todo-app/static/js/main.59c89126.chunk.js"
+  },
+  {
+    "revision": "e911e205635fa9be6a80",
+    "url": "/Todo-app/static/js/1.e911e205.chunk.js"
+  },
+  {
+    "revision": "59c891266339c37df121",
+    "url": "/Todo-app/static/css/main.49a1fee1.chunk.css"
+  },
+  {
+    "revision": "17a1d72c2d20030585b59bffd96325ae",
+    "url": "/Todo-app/index.html"
+  }
+];
